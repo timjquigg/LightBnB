dd6f27397ee85025f179b58e7535c359e9c8b299
Tim Quigg <timjquigg@gmail.com>
2022-11-16 15:55:18 -0700

    Create seed directory with sample data for each table to practice INSERT statements

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS users CASCADE;
+
 DROP TABLE IF EXISTS properties CASCADE;
+
 DROP TABLE IF EXISTS reservations CASCADE;
+
 DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
@@ -12,8 +15,7 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  owner INT REFERENCES users(id) ON DELETE CASCADE,
-  
+  owner_id INT REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumnail_photo_url VARCHAR(255) NOT NULL,
@@ -22,13 +24,11 @@ CREATE TABLE properties (
   parking_spaces INTEGER NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
-  
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
