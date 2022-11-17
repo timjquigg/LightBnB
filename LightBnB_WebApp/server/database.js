72a0a3e5ec4b35900c486700ea44583edb2382ee
Tim Quigg <timjquigg@gmail.com>
2022-11-17 14:38:44 -0700

    Revise addProperty function to be more robust and handle edge cases regarding property parameter

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -217,24 +217,39 @@ exports.getAllProperties = getAllProperties;
  */
 const addProperty = function(property) {
   
-  const queryParams = Object.values(property);
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
   
   const queryString = `
     INSERT INTO properties (
+      owner_id,
       title,
       description,
-      number_of_bedrooms,
-      number_of_bathrooms,
-      parking_spaces,
-      cost_per_night,
       thumbnail_photo_url,
       cover_photo_url,
+      cost_per_night,
       street,
-      country,
       city,
       province,
       post_code,
-      owner_id
+      country,
+      number_of_bedrooms,
+      number_of_bathrooms,
+      parking_spaces
       )
     VALUES (
       $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
